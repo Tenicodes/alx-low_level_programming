64915acb26433b2480ecdff2803fe8db3478e258
tenicodes <yakubufatihatajoke@gmail.com>
2022-09-09 06:18:12 -0700

    go

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,10 +7,12 @@
  *
  * Description: using the main function
  * this program prints "Programming is like building a multilingual puzzle
+ *
  * Return: 0
  */
 int main(void)
 {
 	puts("\"Programming is like a building a multilingual puzzle,\n");
+
 	return (0);
 }
