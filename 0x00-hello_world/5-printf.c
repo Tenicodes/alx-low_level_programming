9ee623e8611b1df4c69a4d72dd70df0620a30490
tenicodes <yakubufatihatajoke@gmail.com>
2022-09-09 05:53:39 -0700

    go

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
- #include <stdio.h>
+#include <stdio.h>
 
- /**
-  * main - Prints a text
-  *
-  * Return: Always 0 (Success)
-  */
- int main(void)
- {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	
+/**
+ * main - Prints a text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");art,\n");
 	return (0);
- }
+}
