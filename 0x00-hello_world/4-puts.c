bbacebbf6bef6b4c1f556093a0c69bc5ed92c93c
tenicodes <yakubufatihatajoke@gmail.com>
2022-09-09 04:57:57 -0700

    go

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
  #include <stdio.h>
+
  /**
  * Description: main - Prints out a string to stdout.
  * Return: 0 if success.
@@ -6,5 +7,6 @@
  int main(void)
  {
  puts(*\"Programming is like building a multilingual puzzle");
+
  return (0);
  }
