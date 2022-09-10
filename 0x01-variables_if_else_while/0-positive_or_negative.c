4eafcef7d1fb36f4cc16bf369e86885a12c5e732
tenicodes <yakubufatihatajoke@gmail.com>
2022-09-10 15:00:41 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,12 +16,11 @@ int main(void)
 	
 	if (n > 0)
 	{
-		prinff("%d iis positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-	{
 	}
 	else
 	{
