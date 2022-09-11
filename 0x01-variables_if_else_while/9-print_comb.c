347bbcaa135b5408c0ca0f7b0155a5e3b78a9dde
tenicodes <yakubufatihatajoke@gmail.com>
2022-09-10 19:17:59 -0700

    k

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *
+ *main - Entry point
+ * Return: Always 0 (Success/Correct)
+ */
+int main(void)
+
+
+{
+iint number;
+
+for (number = 48; number <= 57; number++)
+{
+putchar (number);
+
+if (number == 57)
+{
+btreak;
+}
+
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+
+return (0);
+
+}
