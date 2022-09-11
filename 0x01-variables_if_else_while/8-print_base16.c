eaa2bf791841c348d223148e6d0d61981f05200c
tenicodes <yakubufatihatajoke@gmail.com>
2022-09-10 18:50:27 -0700

    8th

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,15 +10,16 @@ int main(void)
 {
 char base_16;
 
-for (base_16 = 48; base_16 = <=57; base_16++)
-{putchar(base_16);
-}
+	for (base_16 = 48; base_16 = <= 57; base_16++)
+	{
+	putchar(base_16);
+	}
 
-for (base_16 = 97; base _16 <= 102; base_16++)
-{
-putchar(base_16);
-}
-putchar ('\n');
-return (0);
+	for (base_16 = 97; base_16 <= 102; base_16++)
+	{
+	putchar(base_16);
+	}
+	putchar ('\n');
+	return (0);
 
-}
+	}	
