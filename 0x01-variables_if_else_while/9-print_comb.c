9c4324491545dc1dbdfef396ef97690818eb98f6
tenicodes <yakubufatihatajoke@gmail.com>
2022-09-10 20:18:52 -0700

    hi

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 /**
- *  * main - print combinations of numbers
- *   *
- *    * Return: Always 0 (Success)
- *     *
- *      */
+ * main - print combinations of numbers
+ *
+ * Return: Always 0 (Success)
+ *
+ */
 
 int main(void)
 {
-		int n;
+	int n;
 
-			for (n = 48; n <= 57; n++)
-					{
-								putchar(n);
-										if (n != 57)
-													{
-																	putchar(',');
-																				putchar(' ');
-																						}
-											}
-				putchar('\n');
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 
-					return (0);
+	return (0);
 }
