84698c176675b8c55cc315b3b3043ce79b246ca9
tenicodes <yakubufatihatajoke@gmail.com>
2022-09-14 07:22:00 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,14 +14,14 @@ int print_last_digit(int n)
 
 	if (n < 0)
 	{
-		last_digit = (-1 = (n  % 10));
+		last_digit = (-1 * (n  % 10));
 		_putchar (last_digit + '0');
 		return (last_digit);
 	}
 
 	else
 	{
-		last-digit = (n % 10);
+		last_digit = (n % 10);
 		_putchar (last_digit + '0');
 		return (last_digit);
 	}
