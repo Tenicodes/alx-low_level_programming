8bf7dfb264db1e517a4ef386bb7ccd7fe9213eeb
tenicodes <yakubufatihatajoke@gmail.com>
2022-09-14 05:46:40 -0700

    go

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,16 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet(void);
-{
-	int i;
 
-	for (i = 97; <= 122; i++)
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet ='a'; alphabet <= 'z'; alphabet++)
 	{
-		_putchar(i);
+		_putchar(alphabet);
 	}
-	_putchar('\n');
+
+	_putchar('\n')
+
 }
