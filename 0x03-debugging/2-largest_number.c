03abefc619bb50256b735c7890607b9a0e9012c9
Fatihat <95569905+Tenicodes@users.noreply.github.com>
2022-09-15 22:30:19 +0100

    end with new line

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,14 +16,15 @@ int largest_number(int a, int b, int c)
     {
         largest = a;
     }
-    if (b >= a && b >= c)
+    else if (b >= a && b >= c)
     {
         largest = b;
     }
-    if(c >= a && c >= b)
+    else
     {
         largest = c;
     }
 
-	return (largest);
+    return (largest);
+    printf( "\n" );
 }
