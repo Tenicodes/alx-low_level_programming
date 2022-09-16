7fe2dea45a35c728377d8781f6b18b038fc62293
tenicodes <yakubufatihatajoke@gmail.com>
2022-09-16 04:16:51 -0700

    third

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 
 void print_remaining_days(int month, int days, int year)
 {
-	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 !=0))
+	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 	{
 		if (month >= 3 && day >= 60)
 		{
@@ -25,13 +25,12 @@ void print_remaining_days(int month, int days, int year)
 		{
 			else
 		}
-		if(month == 2 && day == 60)
-	}
-	
+		if (month == 2 && day == 60)
+	}	
 		printf("Invalid date: %2d/%02d/%04d\n", month, day - 31, year);
 }
 	else
-	ning_days.c
+{
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 365 - day);
 	}
