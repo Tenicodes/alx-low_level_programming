fa4ecff6a54c76855c251468ee1e639561823c01
tenicodes <yakubufatihatajoke@gmail.com>
2022-09-20 09:16:38 -0700

    go

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,17 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main: program that generates random valid passwords
  * for the program 101-crackme
- * Return: Always 0
+ * Return: 0.
  */
 int main(void)
 {
 	int num;
 
 	srand(time(0));
+
 	num = rand();
+
 	printf("%i\n", num);
 	return (0);
 }
