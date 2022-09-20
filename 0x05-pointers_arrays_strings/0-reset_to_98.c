5a1e6e11b965f341aac00f84ab3f9d0dfb60b2af
tenicodes <yakubufatihatajoke@gmail.com>
2022-09-20 06:17:48 -0700

    zero

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets a integer value to 98
+ * @n: A pointer to an integer that will be reset
+ *
+ * Return: void that means is correct
+ */
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
