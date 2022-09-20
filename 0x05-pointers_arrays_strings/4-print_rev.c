cacbb6b8827da600aa521904783e0e0cb2ef7208
tenicodes <yakubufatihatajoke@gmail.com>
2022-09-20 06:48:09 -0700

    fourth

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in a reverse order
+ * @s: A pointer to an integer that will be reset
+ *
+ * Return: void which means our answer is correct
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (i = i - i ; i >= 0; i--)
+	{
+		_putchar (s[i]);
+	}
+
+	_putchar ('\n');
+}
