1e4f3e5f7a46a7238d6f8fb4059f98c4c19a7989
tenicodes <yakubufatihatajoke@gmail.com>
2022-09-30 09:47:49 -0700

    argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,30 +1,30 @@
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * main - prints the multiplication of two integer followed by a new line
- * @argc: number of arguments passed
- * @argv: array of size argc containing arguments passed
- * Return: 0 on succes, 1 on error.
-*/
-
+  *main - prints result of multiplication followed by a new line.
+  *@argc: number of arguments passed.
+  *@argv: array of size argc containing arguments passed.
+  *
+  *Return: 0 on success.
+  *1 on error.
+  */
 int main(int argc, char *argv[])
 {
 	int count, product;
 
-	product = 1
+	product = 1;
 	if (argc == 3)
 	{
 		for (count = 1; count < argc; count++)
 		{
-			product *=atoi(argv[count]);
+			product *= atoi(argv[count]);
 		}
 		printf("%d\n", product);
 	}
 	else
 	{
-	printf("Error\n");
-	return (1);
+		printf("Error\n");
+		return (1);
 	}
 
 	return (0);
